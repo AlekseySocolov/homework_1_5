55d544c27713996fe21bf9e0e9770df671128185
AlekseySocolov <socolov.alex56@gmail.com>
2019-12-01 21:18:28 +0500

    add atm and quiz

---
+++ test_filemanager.py
@@ -0,0 +1,7 @@
+from function import *
+
+# тестируем функцию выводящую данные о cоздателе
+def test_f_program_creator():
+    assert f_program_creator() == 'Program_creator: Alexey Sokolov from Russia from Orenburg'
+
+
